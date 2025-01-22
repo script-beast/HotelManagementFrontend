7bdc2ff107f16552626e8730d123cfc5dc38b031
Ankit Prajapati <ankitkp028@gmail.com>
2025-01-22 22:43:26 +0530

    backend url updated

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://hotelmanagementbackend-6xfw.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
